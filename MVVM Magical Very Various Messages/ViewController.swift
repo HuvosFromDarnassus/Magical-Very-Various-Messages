6a314cc78a719eb2b2836b2c14214f06ce456d1b
Daniel Tvorun <danieltvorun@icloud.com>
2022-06-22 22:58:20 +0300

    Initial Commit

---
+++ MVVM Magical Very Various Messages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Magical Very Various Messages
+//
+//  Created by Daniel Tvorun on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
